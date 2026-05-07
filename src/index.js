1a8688a9d1c2a76112d395e3b0356da6b97b8e52
Rosa Mae Calvo <balitor.johnrey@dnsc.edu.ph>
2026-05-07 10:15:58 +0800

    Fix index

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ const driverAuthRouter   = require('./routes/driver_auth');
 const operatorAuthRouter = require('./routes/operator_auth');
 const tripsRouter        = require('./routes/trips');
 const subscriptionsRouter = require('./routes/subscriptions');
+const migrateRouter = require('./routes/migrate');
+app.use('/api/migrate', migrateRouter);
 
 app.use('/api/auth',          authRouter);
 app.use('/api/driver',        driverAuthRouter);
